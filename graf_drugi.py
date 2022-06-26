1893552fb79bc97a5cf64016a1d9bb0e00c3ca71
chrisfid <krzysztof.fidyka.dev@gmail.com>
2022-06-26 20:56:46 +0200

    add: working version v2 (without specificity test)

--- graf_drugi.py
+++ graf_drugi.py
@@ -1,44 +1,27 @@
 from igraph import *
 
-# make_bipartite_graph(types, edges, directed = FALSE)
-# g = Graph()
-# g.make_full_bipartite_graph(
-#   2,
-#   2,
-#   directed = False,
-#   mode = "all"
-# )
-# g.sample_bipartite(10, 5, p=1)
-# g.vs
-g = Graph.Bipartite([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], [(0,8), (1,8), (2,9), (3,10), (4,11), (5,12), (7,13)])
-# g = Graph.Bipartite([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], [(0, 1), (2, 3), (0, 3)])
-# g = Graph.Bipartite([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], [(0,8), (1,8), (2,9), (3,10), (4,11), (5,12), (7,13), (6,13)])
+students = ["Dominik", "Jakub", "Maciej", "Michał", "Kacper"]
+classes = ["Modelowanie", "Metodyka pracy naukowej", "Programowanie", "Sieci"]
 
-players = ["Lionell Messi", "Neymar", "Cristiano Ronaldo", "Robert Lewandowski", "P.E. Aubameyang", "Karim Benzema", "Gareth Bale", "Zlatan Ibrahimovic"]
-clubs = ["PSG", "Manchester Utd.", "Bayern", "FC Barcelona", "Real Madryt", "AC Milan"]
-# g = Graph.Bipartite(players + clubs, [(0,8), (1,8), (2,9), (3,10), (4,11), (5,12), (7,13)])
+arrangement = []
+for i in range(len(students)):
+    arrangement.append(0)
+for i in range(len(classes)):
+    arrangement.append(1)
 
-g.vs["name"] = players + clubs
+g = Graph.Bipartite(
+    arrangement,
+    [(0, 5), (1, 6), (1, 7), (2, 5), (2, 8), (3, 6), (4, 5), (4, 6)]
+)
+g.vs["label"] = students + classes
+g.vs["color"] = ["pink" if x else "green" for x in arrangement]
 
+# Analiza skupień
+print("Analiza skupień:")
+print(g.community_edge_betweenness().as_clustering())
+print(g.edge_betweenness())
 
-# g.es["age"] = [25, 31, 18, 47, 22, 23, 50]
-arr = []
-for i in range(len(players)):
-    arr.append("m")
-for i in range(len(clubs)):
-    arr.append("f")
-
-g.vs["gender"] = arr
-print(g.degree())
-print(g)
-
-
-# layout = g.layout("kk")
+EDGE_DIVIDE_RATIO = 5
+edge_widths = list(map(lambda x: x / EDGE_DIVIDE_RATIO, g.edge_betweenness()))
 layout = g.layout_bipartite()
-# plot(g, layout=layout)
-g.vs["label"] = g.vs["name"]
-color_dict = {"m": "blue", "f": "pink"}
-g.vs["color"] = [color_dict[gender] for gender in g.vs["gender"]]
-plot(g, layout=layout, bbox=(600, 300), margin=20)
-# layout = g.layout("fr")
-# plot(g, layout=layout)
+plot(g, layout=layout, bbox=(600, 300), margin=20, edge_width=edge_widths)
--- graf_pierwszy.py
+++ graf_pierwszy.py
@@ -6,17 +6,14 @@ from igraph import *
 # g.add_edges([(0,1)])
 
 g = Graph.Famous('Octahedral')
-layout = g.layout("kk")
-plot(g, layout=layout)
 
-# Centralność (stopień węzła)
-BSC = g.degree()
-print(f'Centralność (stopień węzła) {max(BSC)}')
+# Zwraca liczbę połączeń wszystkich węzłów
+v_degree = g.degree()
 
-# Centralność (bliskość jednostki do innych jednostek)
-print(f'Centralność (bliskość jednostki do innych jednostek) {max(BSC) / (len(BSC) - 1)}')
+print(f'Centralność (stopień węzła) {max(v_degree)}')
 
-# promień średnica gęstość
+print(f'Centralność (bliskość jednostki do innych jednostek) {max(v_degree) / (len(v_degree) - 1)}')
+
 print(f'Promień: {g.radius()}')
 print(f'Średnica: {g.diameter()}')
 print(f'Gęstość: {g.density()}')
@@ -27,11 +24,21 @@ if (len(components) == 1):
     print('Graf jest spójny')
 else:
     print('Graf nie jest spójny')
-    print(f'Części grafu\n{components}')
+    print(f'Części grafu:\n{components}')
 
-# It is ignored for undirected graphs.
+# Wydzielenie węzłów które są mocno połączone 
 if (g.is_directed()):
     strongy_connected = g.components(mode="strong")
     print(f'Węzły mocno połączone: {strongy_connected}')
 else:
     print('Graf jest nieskierowany, zatem wydzielenie węzłów które są mocno połączone nie jest możliwe')
+
+layout = g.layout("kk")
+g.vs["label"] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+plot(
+    g,
+    layout=layout, 
+    edge_width=g.edge_betweenness(), 
+    vertex_size = [vd * 10 + 5 for vd in v_degree],
+ )
